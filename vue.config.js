9557553eb5450f26606446c3da2538c1d1359612
Anita Chen <53967230+Tingching0602@users.noreply.github.com>
2022-02-17 15:45:15 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -5,12 +5,7 @@ module.exports = {
   publicPath: '.',
   pages: {
     index: {
-      // entry for the page
       entry: "src/main.js",
-      // the source template
-      // template: "public/index.html",
-      // output as dist/index.html
-      // filename: "index.html"
     }
   },
   configureWebpack: {
@@ -25,7 +20,7 @@ module.exports = {
       }
     }
   },
-  // 這段是設定開發人員工具中，可以找的到編譯後的CSS規則來源寫在哪
+
   css: {
     sourceMap: true,
     loaderOptions: {
